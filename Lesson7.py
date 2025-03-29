d543c3024480886d0f107b0471f1440c79d7f52f
vofjd <bykovskijila227@gmail.com>
2025-03-29 16:28:53 +0200

    Lesson7 generator

--- Lesson7.py
+++ Lesson7.py
@@ -38,35 +38,91 @@
 from collections import Counter
 
 
-class counter:
-    def __init__(self, max_number):
-        self.i = 0
-        self.max_number = max_number
+# class counter:
+#     def __init__(self, max_number):
+#         self.i = 0
+#         self.max_number = max_number
+#
+#     def __iter__(self):
+#         self.i = 0
+#         return self
+#
+#     def __next__(self):
+#         self.i += 1
+#         if self.i > self.max_number:
+#             raise StopIteration
+#         return self.i
+#
+#
+#
+#
+# count = counter(5)
+# for elem in count:
+#     print(elem)
+#
+#
+#
+#
+# print("-"*30)
+# count = counter(5)
+# print(count.__next__())
+# print(count.__next__())
+# print(next(count))
+#
+#
 
-    def __iter__(self):
-        self.i = 0
-        return self
-
-    def __next__(self):
-        self.i += 1
-        if self.i > self.max_number:
-            raise StopIteration
-        return self.i
 
 
+#list generator
+# list_power_2 = [i **2 for i in range(1, 20, 1)]
+# print(list_power_2)
+
+# """генератори в пайтон це функція,
+# що повертає ітератор, який під час ітерації повертає послідовність значень"""
+#
+#
+#
+# def raise_to_the_degrees(number):
+#     i = 0
+#     # for _ in range(max_degrees):
+#     while True:
+#         result = number ** i
+#         yield  result
+#         yield number ** i
+#         i += 1
+#         if result > 100 ** 20:
+#             return
+#         i += 1
+#
+#
+#
+#
+#
+# res = raise_to_the_degrees(2)
+# print(res)
+#
+# for elem in res:
+#     print(elem)
 
 
-count = counter(5)
-for elem in count:
-    print(elem)
 
 
 
 
-print("-"*30)
-count = counter(5)
-print(count.__next__())
-print(count.__next__())
-print(next(count))
 
 
+
+
+
+class Helper:
+    def __init__(self, work):
+        self.work = work
+
+
+    def __call__(self, work):
+        return f"I will help you with your {self.work}. Afterwarsd I will help you with {work}"
+
+
+
+hepler = Helper("homework")
+print(hepler("cleaning"))
